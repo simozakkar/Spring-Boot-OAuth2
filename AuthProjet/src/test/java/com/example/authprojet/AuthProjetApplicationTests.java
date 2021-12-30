3065db85bb90fe7b798c01bfab011afff96878ef
EZ-ZAKKAR MOHAMMED <simozakkar>
2021-12-30 11:12:40 +0100

    First Commit

---
+++ AuthProjet/src/test/java/com/example/authprojet/AuthProjetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authprojet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthProjetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
